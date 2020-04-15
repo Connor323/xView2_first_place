dc5cb0efc1f246dac6d14e37d5e93e3e7b6416bf
hao66 <hao66@purdue.edu>
2020-04-14 20:52:10 -0500

    change gpu device

--- train154cls.sh
+++ train154cls.sh
@@ -1,8 +1,8 @@
 echo "training senet154 classification model with seeds 0-2"
-python train154_cls_cce.py 0 1
-python train154_cls_cce.py 1 1
-python train154_cls_cce.py 2 1
-python tune154_cls_cce.py 0 1
-python tune154_cls_cce.py 1 1
-python tune154_cls_cce.py 2 1
+python train154_cls_cce.py 0 0
+python train154_cls_cce.py 1 0
+python train154_cls_cce.py 2 0
+python tune154_cls_cce.py 0 0
+python tune154_cls_cce.py 1 0
+python tune154_cls_cce.py 2 0
 echo "Model senet154 classification trained!"
--- train92cls.sh
+++ train92cls.sh
@@ -1,8 +1,8 @@
 echo "training dpn92 classification model with seeds 0-2"
-python train92_cls_cce.py 0 1
-python train92_cls_cce.py 1 1
-python train92_cls_cce.py 2 1
-python tune92_cls_cce.py 0 1
-python tune92_cls_cce.py 1 1
-python tune92_cls_cce.py 2 1
+python train92_cls_cce.py 0 0
+python train92_cls_cce.py 1 0
+python train92_cls_cce.py 2 0
+python tune92_cls_cce.py 0 0
+python tune92_cls_cce.py 1 0
+python tune92_cls_cce.py 2 0
 echo "Model dpn92 classification trained!"
